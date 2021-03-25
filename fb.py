33d71d17dd7aacce275c9bd545888f356f3ab841
kingwangzzang1234 <wychoi0408@naver.com>
2021-03-25 06:43:41 +0000

    feat: Print fizz if i is times of 3

--- fb.py
+++ fb.py
@@ -1,6 +1,9 @@
 def do_fizzbuzz(num):
     for i in range(1,num+1):
-        print(i)
+        if i%3==0:
+            print('fizz')
+        else:
+            print(i)
 
 if __name__=='__main__':
     user_num = int(input('Type the number: '))
