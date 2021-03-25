6d625d816902d1cb028a29537122ea9615b5374e
kingwangzzang1234 <wychoi0408@naver.com>
2021-03-25 06:42:12 +0000

    feat: Init do_fizzbuzz
    user_num got from user.
    print 1 to user_num to test it works

--- fb.py
+++ fb.py
@@ -1,5 +1,7 @@
-def test():
-    print('hello')
+def do_fizzbuzz(num):
+    for i in range(1,num+1):
+        print(i)
 
 if __name__=='__main__':
-    test()
+    user_num = int(input('Type the number: '))
+    do_fizzbuzz(user_num)
