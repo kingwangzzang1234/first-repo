98ee323062d28fb6c7f9c5ea12c48d8aad6c83e0
kingwangzzang1234 <wychoi0408@naver.com>
2021-03-25 06:48:53 +0000

    refactor: Do fizzbuzz with list comprehension

--- fb.py
+++ fb.py
@@ -1,14 +1,11 @@
 def do_fizzbuzz(num):
-    for i in range(1,num+1):
-        if i%15==0:
-            print('fizzbuzz')
-        elif i%3==0:
-            print('fizz')
-        elif i%5==0:
-            print('buzz')
-        else:
-            print(i)
-
+    result = [
+        'fizzbuzz' if i%15==0 else
+        'fizz' if i%3==0 else
+        'buzz' if i%5==0 else
+        i for i in range(1,num+1)
+    ]
+    print(result)
 if __name__=='__main__':
     user_num = int(input('Type the number: '))
     do_fizzbuzz(user_num)
